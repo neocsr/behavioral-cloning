800c0ead6927a6556de34b19dce0dd60b00c46ce
Christian <neocsr@gmail.com>
2017-01-08 02:42:22 -0800

    Add final model

--- drive.py
+++ drive.py
@@ -36,12 +36,20 @@ def telemetry(sid, data):
     # The current image from the center camera of the car
     imgString = data["image"]
     image = Image.open(BytesIO(base64.b64decode(imgString)))
+
+    # Crop and resize
+    image = image.crop((0, 80, 320, 140))
+    image = image.resize((128, 64), Image.ANTIALIAS)
+
+
     image_array = np.asarray(image)
     transformed_image_array = image_array[None, :, :, :]
     # This model currently assumes that the features of the model are just the images. Feel free to change this.
     steering_angle = float(model.predict(transformed_image_array, batch_size=1))
     # The driving model currently just outputs a constant throttle. Feel free to edit this.
-    throttle = 0.2
+
+    throttle = 0.5
+
     print(steering_angle, throttle)
     send_control(steering_angle, throttle)
 
